7acc8372123fd98360c3150a801f151c8388170f
王涛 <wangtao5515@cvte.com>
2018-11-20 19:54:19 +0800

    提交弹幕测试内容

--- socket/index.js
+++ socket/index.js
@@ -1,25 +1,23 @@
 const authorization = require("./authorization");
 
-const {
-  connection,
-  login,
-  disconnect,
-  operation,
-} = require("./event");
+const { connection, login, disconnect, operation } = require("./event");
 const IO = require("./single/io");
 
 module.exports = http => {
   IO.initIO(http);
 
   // IO.io.set('authorization', authorization);
-  IO.io.on("connection", function (socket) {
+  IO.io.on("connection", function(socket) {
     console.log("a user connected");
 
     //监听新用户加入
     connection(socket)();
     //监听用户登录
     socket.on("login", login(socket));
-
+    socket.on("danmaku", comment => {
+      console.log("danmaku", comment);
+      socket.emit("danmaku", comment);
+    });
     //监听用户退出
     socket.on("disconnect", disconnect(socket));
     //监听用户发出操作
